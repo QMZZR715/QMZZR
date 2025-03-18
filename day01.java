c97d03a3b85b15765d1ddd003f9fb3480143d28a
QMZZR715 <368260440@qq.com>
2025-03-18 16:46:17 +0800

    Add files via upload

---
+++ day01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ScannerDemo{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ÇëÊäÈëÕûÊı");
+		int i=sc.nextInt();
+		System.out.println(i);
+	}
+}
