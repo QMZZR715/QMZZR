6907ec2bfbd9f564f650f3fcd4d83716a67487c4
QMZZR715 <368260440@qq.com>
2025-03-18 16:49:39 +0800

    Update day01.java

--- day01.java
+++ day01.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ScannerDemo{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("ÇëÊäÈëÕûÊı");
+		System.out.println("è¯·è¾“å…¥ä¸€ä¸ªæ•´æ•°ï¼š");
 		int i=sc.nextInt();
 		System.out.println(i);
 	}
